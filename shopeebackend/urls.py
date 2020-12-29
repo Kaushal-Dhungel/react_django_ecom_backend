8447a0a6381842ab926f82625f76b36357f44de9
Kaushal <hazardfanboy91@gmail.com>
2020-12-29 09:31:24 +0530

    fixed urlpatterns

--- shopeebackend/urls.py
+++ shopeebackend/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('', include('shopee.urls')),
 ]
 
-if settings.DEBUG is True:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# if settings.DEBUG is True:
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
